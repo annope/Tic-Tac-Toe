6d16172828a73a858b4c9de42f585b810db988df
Ann Opeyemi <annopeyemi@gmail.com>
2023-10-03 17:11:39 -0400

    Adding toggle ability to menu button

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log("Hello World")
+const App = {
+    //All of ours selected HTML elements
+    $: {
+        menu: document.querySelector('.menu'),
+        menuItems: document.querySelector(".items"),
+
+    }
+}
+
+
+
+const menu = document.querySelector('.menu')
+const menuItems = menu.querySelector(".items");
+
+menu.addEventListener("click", (event) =>{
+    menuItems.classList.toggle("hidden");
+});
