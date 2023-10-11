a6a37e99d46e945930e15b4d1d2440223109d503
Ann Opeyemi <annopeyemi@gmail.com>
2023-10-11 16:57:56 -0400

    Updating state of user's wins

--- js/app.js
+++ js/app.js
@@ -6,12 +6,44 @@ const App = {
         resetBtn: document.querySelector('[data-id="reset-btn"]'),
         newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
         squares: document.querySelectorAll('[data-id="square"]'),
+        modal: document.querySelectorAll('[data-id="modal"]'),
+        modalText: document.querySelectorAll('[data-id="modal-text"]'),
+        modalBtn: document.querySelectorAll('[data-id="modal-btn"]'),
 
     },
 
     state: {
-        currentPlayer: 1,
         moves: [],
+    },
+
+    getGameStatus(moves){
+         const p1Moves = moves.filter(move => move.playerId === 1).map(move => +move.squareId)
+         const p2Moves = moves.filter(move => move.playerId === 2).map(move => +move.squareId)
+
+         const winningPatterns = [
+                    [1, 2, 3],
+                    [1, 5, 9],
+                    [1, 4, 7],
+                    [2, 5, 8],
+                    [3, 5, 7],
+                    [3, 6, 9],
+                    [4, 5, 6],
+                    [7, 8, 9],
+                ];
+            let winner = null
+
+            winningPatterns.forEach(patterns => {
+                const p1Wins = pattern.every(v => p1Moves.includes(v))
+                const p2Wins = pattern.every(v => p2Moves.includes(v))
+
+                if (p1Wins) winner = 1
+                if (p2Wins) winner = 2
+            })
+
+        return{
+            status: 'in-progress', //in-progress | complete
+            winner: 1 // 1 | 2 | null
+        }
     },
 
     init() {
@@ -41,12 +73,22 @@ const App = {
                 console.log('Current player is ${App.state.currentPlayer}');
 
                 //Check if there is already a play, if so, return early
+                const hasMove = (squareId) => {
+                    const existingMove = App.state.moves.find(move => move.squareId === squareId)
+                    return existingMove !== undefined
+                }
+                
                 if (square.hasChildNodes()){
                     return;
                 }
 
                 //Determine which player icon to add to the square
-                const currentPlayer = App.state.currentPlayer
+                const lastMove = App.state.moves.at(-1);
+                const getOppositePlayer = (playerId) => (playerId === 1 ? 2 : 1);
+                const currentPlayer =
+                    App.state.moves.length === 0
+                    ? 1 
+                    : getOppositePlayer(lastMove.playerId);
 
                 const icon = document.createElement("i");
 
@@ -56,23 +98,25 @@ const App = {
                     icon.classList.add("fa-solid", "fa-o", "turquoise");
                 }
 
-                App.state.currentPlayer = App.state.currentPlayer === 1 ? 2 : 1;
+                App.state.moves.push({
+                    squareId: +square.id,
+                    playerId: currentPlayer,
+                })
 
-                
-                
-
+          
                 square.replaceChildren(icon);
                 //Check if there is a winner or tie game
-                const winningPatterns = [
-                    [1, 2, 3],
-                    [1, 5, 9],
-                    [1, 4, 7],
-                    [2, 5, 8],
-                    [3, 5, 7],
-                    [3, 6, 9],
-                    [4, 5, 6],
-                    [7, 8, 9],
-                ];
+
+                const status = App.getGameStatus(App.state.moves);
+
+                if (game.status === "complete"){
+                    if(game.winner){
+                        alert('Player ${game.winner} wins!');
+                    } else {
+                        alert("Tie!");
+                    }
+                }
+               
             });
         });
 
